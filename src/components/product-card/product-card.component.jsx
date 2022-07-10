8130ce82cc8d568a3d2e7530fb823b11169ccbf1
hiren <hirenvadher954@gmail.com>
2022-07-11 00:04:02 +0530

    checkout functionality added

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,18 +1,21 @@
 import './product-card.styles.scss';
 
-import React from 'react';
+import React, {useContext} from 'react';
 import Button from "../button/button.component";
+import {CartContext} from "../../contexts/cart.context";
 
 const ProductCard = ({product}) => {
-    const { name, price, imageUrl } = product;
+    const {name, price, imageUrl} = product;
+    const {addItemToCart} = useContext(CartContext);
+
     return (
         <div className='product-card-container'>
-            <img src={imageUrl} alt={`${name}`} />
+            <img src={imageUrl} alt={`${name}`}/>
             <div className='footer'>
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
-            <Button buttonType='inverted'>Add to card</Button>
+            <Button buttonType='inverted' onClick={() => addItemToCart(product)}>Add to card</Button>
         </div>
     );
 };
